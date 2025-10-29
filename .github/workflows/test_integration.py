dc70f770027eb2ff8d8ed4751fb32889777a0fe5
Biju Dash <bijudas366@gmail.com>
2025-10-29 15:35:41 +0800

    Delete .github/workflows/test_integration.py

--- .github/workflows/test_integration.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from app import register, login
-from unittest.mock import patch
-
-@pytest.mark.integration
-def test_register_then_login(monkeypatch):
-    monkeypatch.setenv("ADMIN_PASS", "123")
-    register("admin@example.com", "123")
-    assert login("admin@example.com", "123") is True

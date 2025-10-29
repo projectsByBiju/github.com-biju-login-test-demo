2c12857dcc1ccb5112280849f34cf1bd6519fa85
Biju Dash <bijudas366@gmail.com>
2025-10-29 15:41:34 +0800

    Delete .github/workflows/test_unit.py

--- .github/workflows/test_unit.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from app import login
-
-@pytest.mark.unit
-def test_login_success(monkeypatch):
-    monkeypatch.setenv("ADMIN_PASS", "secret123")
-    assert login("admin@example.com", "secret123") is True
